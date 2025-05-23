0aa56813ca56770d5d27475787ef2eb6055e2285
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 11:50:37 -0300

    Adiciona metodo sort e reverse

--- exemplo-sort&reverse.js
+++ exemplo-sort&reverse.js
@@ -11,3 +11,18 @@ let playlist1 = ["Rap", "Pop", "Trap", "R/B", "Hip-Hop"];
 let playlistInvertida = playlist.reverse();
 
 console.log("Playlist invertida:", playlistInvertida);
+
+//exemplo sort 
+let numeros = [10, 5, 100, 50, 1];
+
+console.log("Antes do sort:", numeros);
+
+//Ordenando os números de forma crescente 
+let numerosOrdenadosCrescentes = numeros.sort((a, b) => a - b);
+console.log("Números em ordem crescente:", numerosOrdenadosCrescentes);
+
+//Ordenando os números de forma decrescente
+let numerosOrdenadosDecrescentes = numeros.sort((a, b) => b - a);
+console.log("Números em ordem decrescente:", numerosOrdenadosDecrescentes);
+
+
