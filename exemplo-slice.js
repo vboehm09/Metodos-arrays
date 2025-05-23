4b9fb34b4d968ff07b811c6cb1bfaf70bdf1fa18
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 09:56:06 -0300

    Adiciona metodo slice

---
+++ exemplo-slice.js
@@ -0,0 +1,6 @@
+//SLICE
+let playlist = ["Rap", "Pop", "Trap", "R/B", "Hip-Hop", "Funk", "Rock", "Reggae"];
+console.log("Parte da playlist (índices 2 a 5):", playlist.slice(2, 6)); //['Trap', 'R/B', 'Hip-Hop', 'Funk']
+
+console.log("Parte da playlist(índices 0 a 3):", playlist.slice(0, 4)); //['Rap', 'Pop', 'Trap', 'R/B']
+
