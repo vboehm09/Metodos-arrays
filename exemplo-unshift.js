50e54e5e33b0cc90c6481275979dc284716480ef
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 08:58:38 -0300

    Adiciona exemplo unshift

---
+++ exemplo-unshift.js
@@ -0,0 +1,11 @@
+//UNSHIFT 
+let playlist = ["Rap", "Pop", "Trap"];
+
+console.log("Antes do unshift:", playlist);
+
+//Adicionando uma nova música no inicio da playlist 
+playlist.unshift("Hip-Hop");
+
+console.log("Depois do unshift:", playlist);
+
+
