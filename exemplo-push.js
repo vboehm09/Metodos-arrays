070dbc9fe57ed1b9d837c46d33aba24a448ed852
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 08:54:34 -0300

    Adiciona exemplo push

---
+++ exemplo-push.js
@@ -0,0 +1,14 @@
+//PUSH
+let playlist = ["Rap", "Pop", "Trap"];
+
+console.log(`Antes do push:`, playlist);
+
+//Adicionando uma nova música à playlist
+playlist.push("R/B");
+
+console.log(`Depois do push:`, playlist);
+
+playlist.push("Hip-Hop");
+
+console.log(`Depois do push:`, playlist);
+
