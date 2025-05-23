49642eaa0daf0b178ac487fd0c50dd1a055d58c8
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 09:28:49 -0300

    Adiciona metodo include

---
+++ exemplo-include.js
@@ -0,0 +1,6 @@
+//INCLUDE
+let playlist = ["Rap", "Pop", "Trap", "R/B", "Hip-Hop", "Funk", "Rock"]
+
+console.log("A playlist inclui Rap?", playlist.includes("Rap"));
+console.log("A playlist inclui Pop?", playlist.includes("Pop"));
+
