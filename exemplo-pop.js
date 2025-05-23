098aab82aef8485aa86e8e7c6b14bf4e4f9f19f0
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 08:54:09 -0300

    Adiciona exemplo pop

---
+++ exemplo-pop.js
@@ -0,0 +1,14 @@
+//POP
+let playlist = ["Rap", "Pop", "Trap"];
+
+console.log("Antes do pop:", playlist);
+
+//Removendo a última música da playlist 
+let músicaRemovida = playlist.pop();
+
+console.log("Depois do pop:", playlist);
+console.log("Música removida:", músicaRemovida);
+
+let segundaMusicaRemovida = playlist.pop();
+console.log("Depois do pop:", playlist);
+console.log("Música removida:", segundaMusicaRemovida);
