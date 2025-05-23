c6e4c4fff8aec2ef9c4e187f6a9a4f2cd65ac77e
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 11:28:17 -0300

    Adiciona metodo filter

---
+++ exemplo-filter.js
@@ -0,0 +1,7 @@
+//FILTER
+let playlist = ["Rap", "Pop", "Trap", "Hip-Hop", "R/B"];
+
+let playlistFiltrada = playlist.filter(musica => musica.length > 4);
+
+console.log(`Playlist filtrada (músicas com mais de 4 letras): ${playlistFiltrada}`);
+
