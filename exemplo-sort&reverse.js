edce5ba30b05b0f0fb48f9f598587b28b4fe37a6
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 11:41:55 -0300

    Adiciona metodo sort e reverse

---
+++ exemplo-sort&reverse.js
@@ -0,0 +1,13 @@
+//SORT
+let playlist = ["Rap", "Pop", "Trap", "R/B", "Hip-Hop"];
+
+let playlistOrdenada = playlist.sort();
+
+console.log("Playlist ordenada:", playlistOrdenada);
+
+//REVERSE
+let playlist1 = ["Rap", "Pop", "Trap", "R/B", "Hip-Hop"];
+
+let playlistInvertida = playlist.reverse();
+
+console.log("Playlist invertida:", playlistInvertida);
