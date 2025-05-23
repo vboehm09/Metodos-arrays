a07426f4e3b6f72212fffb3951687bd83cc87019
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 10:50:40 -0300

    Adiciona metodo concat

---
+++ exemplo-concat.js
@@ -0,0 +1,6 @@
+//CONCAT 
+let playlist1 = ["Rap", "Pop", "Trap"];
+let playlist2 = ["Hip-Hop", "R/B"];
+
+console.log(`Playlist combinada: ${playlist1.concat(playlist2)}`);
+console.log(`Playlist combinada: ${playlist1.concat(playlist2).join(', ')}`);
