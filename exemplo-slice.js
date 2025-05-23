4ddbfe3e20438aa978b37279d7349c9305d22042
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 10:04:31 -0300

    Adiciona metodo slice

--- exemplo-slice.js
+++ exemplo-slice.js
@@ -4,3 +4,4 @@ console.log("Parte da playlist (índices 2 a 5):", playlist.slice(2, 6)); //['Tr
 
 console.log("Parte da playlist(índices 0 a 3):", playlist.slice(0, 4)); //['Rap', 'Pop', 'Trap', 'R/B']
 
+console.log(`Apenas: ${playlist.slice(6,7)}`);
