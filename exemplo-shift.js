4382d0094b1afb208ebd28815ef2f1a0744e1920
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 08:55:10 -0300

    Adiciona exemplo shift

---
+++ exemplo-shift.js
@@ -0,0 +1,12 @@
+//SHIFT 
+let playlist = ["Rap", "Pop", "Trap"];
+
+console.log("Antes do shift:", playlist);
+
+//Removendo a primeira música da playlist
+let musicaRemovida = playlist.shift();
+
+console.log("Depois do shift:", playlist);
+console.log("Música removida:", musicaRemovida);
+
+
