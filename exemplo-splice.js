1341e6702f110aa3512d538bffc2a3363c55b4ab
Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-23 10:44:25 -0300

    Adiciona metodo splice

--- exemplo-splice.js
+++ exemplo-splice.js
@@ -1,11 +1,13 @@
 //SPLICE 
-let playlist = ["Rap", "Pop", "Trap", "R/B", "Hip-Hop"];
+let playlist = ["Rap", "Pop", "Trap", "R/B", "Hip-Hop", "Hino"];
 
 console.log(`Antes do splice: ${playlist}`);
 
 //Removendo 2 elementos a partir do índice 2
-let elementosRemovidos = playlist.splice(2, 2);
+//let elementosRemovidos = playlist.splice(2, 2);
+let desafio = playlist.splice(2, 2 );
 
-console.log(`Depois do splice: ${playlist}`);
-console.log(`Elementos removidos: ${elementosRemovidos}`);
+//console.log(`Depois do splice: ${playlist}`);
+//console.log(`Elementos removidos: ${elementosRemovidos}`);
+console.log(`Elementos removidos: ${desafio}`);
 
